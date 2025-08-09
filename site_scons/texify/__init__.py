16f837613cd8396df4da71ccc7d28f02eb2d7740
Nathan Gober <ngober@protonmail.com>
2025-08-08 22:15:50 -0500

    Used LaTeX dirtytalk for quotes

--- site_scons/texify/__init__.py
+++ site_scons/texify/__init__.py
@@ -2,6 +2,7 @@ import collections
 import re
 import itertools
 import functools
+import operator
 
 def smallcap(text):
     return re.sub(r'\b[A-Z]{2,}\b', lambda match: f'\\textsc{{{match.group(0).title()}}}', text)
@@ -10,7 +11,7 @@ def drop_bracket(text):
     return re.sub(r'\[{1,2}((.|\n)*)\]{1,2}', r'\1', text, flags=re.MULTILINE)
 
 def split_paragraph(text):
-    return [list(p) for k,p in itertools.groupby(text.splitlines(), key=lambda l: l.rstrip() != '') if k]
+    return ['\n'.join(p) for k,p in itertools.groupby(text.splitlines(), key=lambda l: l.rstrip() != '') if k]
 
 def indent_counts(lines):
     return [len(re.search('\s*', l).group(0)) for l in lines]
@@ -32,6 +33,38 @@ def indentations_for(par):
                 return ''
             return map(ident, counts)
     return itertools.repeat('')
+
+DOUBLE_QUOTED_STRING = re.compile(r'[“"]((?s:.)+?)[”"]', re.M)
+SINGLE_QUOTED_STRING = re.compile(r"[‘']((?s:.)+?)[’']", re.M)
+
+def fix_single_quote_instance(par):
+    par = re.sub(SINGLE_QUOTED_STRING, lambda match: f'\say{{{ match.group(1) }}}', par, count=1)
+    par = re.sub(DOUBLE_QUOTED_STRING, lambda match: f'\say{{{ match.group(1) }}}', par, count=1)
+    return par
+
+def produce(func, initial):
+    while True:
+        initial = func(initial)
+        yield initial
+
+def fix_quotes(par):
+    fix_sequence = produce(fix_single_quote_instance, par)
+    fix_sequence = itertools.dropwhile(lambda args: args[0] != args[1], itertools.pairwise(fix_sequence))
+    fix_sequence = map(operator.itemgetter(1), fix_sequence)
+
+    result = next(fix_sequence)
+
+    with_prefix = f'"{result}'
+    result_with_prefix = fix_single_quote_instance(with_prefix)
+    if result_with_prefix != with_prefix:
+        return result_with_prefix
+
+    with_suffix = f'{result}"'
+    result_with_suffix = fix_single_quote_instance(with_suffix)
+    if result_with_suffix != with_suffix:
+        return result_with_suffix
+
+    return result
 
 RIGHT_SELAH = '''
 
@@ -60,20 +93,25 @@ def identity(x):
 
 def process_par(par, func=identity):
     # Drop [brackets] used by translations to indicate uncertain readings
-    par = [drop_bracket(l) for l in par]
+    par = drop_bracket(par)
 
-    par = func(par)
+    lines = par.splitlines()
+
+    lines = func(lines)
 
     # Push 'Selah' in psalms to the right and italicize
-    par[-1] = re.sub('Selah$', RIGHT_SELAH, par[-1])
+    lines[-1] = re.sub('Selah$', RIGHT_SELAH, lines[-1])
 
     # Change all-caps words (e.g. LORD) to smallcap style
-    par = [smallcap(l) for l in par]
-
-    # FIXME use LaTeX's dirtytalk package to ensure quotes are good
+    lines = [smallcap(l) for l in lines]
 
     # Join lines together again
-    return '\n'.join(' '.join(l) for l in zip(indentations_for(par), par))
+    par = '\n'.join(' '.join(l) for l in zip(indentations_for(lines), lines))
+
+    # Use LaTeX's dirtytalk package to ensure quotes are good
+    par = fix_quotes(par)
+
+    return par
     
 def reading_join_text(address, text, **kwargs):
     parfuncs = itertools.chain([functools.partial(config_par_adjust, **kwargs)], itertools.repeat(identity))
